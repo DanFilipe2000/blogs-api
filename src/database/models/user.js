1b44c63936b9f18517804100c63928e95bc41f43
Daniel Filipe <anielilipe90@gmail.com>
2022-08-30 23:39:04 -0300

    Requisito 02

---
+++ src/database/models/user.js
@@ -0,0 +1,19 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  })
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+        { as: 'blogPost', foreignKey: 'userId' }
+    )
+  }
+
+  return User;
+};
+
+module.exports = User;
